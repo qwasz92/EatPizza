3b8ec900ee65617567885fa7750cf06af76a6bfb
Hsuan-Yi,Lin <lovevul35j6@gmail.com>
2020-05-04 16:01:58 +0800

    首先定義一個不變的PIZZA_ID。再將recyclerView建立出來，並是一個layoutManager。定義一個能創作者資料可以轉出的的intent，最後再將資料轉出道myAdapter裡面。 建立一個MainAdapte資料庫

--- app/src/main/java/com/example/eatpizza/MainActivity.kt
+++ app/src/main/java/com/example/eatpizza/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.eatpizza
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+val PIZZA_ID = "PIZZA_ID"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = pizzaRecyclerView
+        recyclerView.layoutManager = LinearLayoutManager (this,LinearLayoutManager.VERTICAL,false)
+
+        val launchCreatorActivity ={ id:Int? ->
+            val intent = Intent(this,CreatorActivity::class.java)
+            intent.putExtra(PIZZA_ID,id)
+            startActivity(intent)
+        }
+        val myAdapter = MainAdapter(launchCreatorActivity)
     }
 }
