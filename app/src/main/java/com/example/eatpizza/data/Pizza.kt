c7fd4128e1a36d24375df1520bfd8e41c5274f23
Hsuan-Yi,Lin <lovevul35j6@gmail.com>
2020-05-09 23:41:37 +0800

    Pizza建立資料關鍵值。PizzaDao建立取得全部資料、取得資料ID、刪除資料。Activity_creator資料修正&Activity_menu的資料名稱修正

--- app/src/main/java/com/example/eatpizza/data/Pizza.kt
+++ app/src/main/java/com/example/eatpizza/data/Pizza.kt
@@ -1,2 +1,13 @@
 package com.example.eatpizza.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NotNull
+
+@Entity
+class Pizza(    @PrimaryKey(autoGenerate = true)
+                var id:Int?,
+                val name:String
+){
+}
--- app/src/main/java/com/example/eatpizza/data/PizzaDao.kt
+++ app/src/main/java/com/example/eatpizza/data/PizzaDao.kt
@@ -1,2 +1,22 @@
 package com.example.eatpizza.data
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+    interface PizzaDao{
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(pizza: Pizza)
+
+    @Query("select * from pizza")
+    fun getAll():List<Pizza>
+
+    @Query("select * from pizza where id = :id")
+    fun getPizzaByid(id:Int):Pizza
+
+    @Query("delete from pizza where id = :id")
+    fun deletePizzaById(id:Int)
+}
