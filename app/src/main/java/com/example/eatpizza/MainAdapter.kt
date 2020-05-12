fbe516a221991d24c490c83fca326dbde1acecf1
Hsuan-Yi,Lin <lovevul35j6@gmail.com>
2020-05-12 15:13:46 +0800

    修正mainAvtivity onCreateViewHolder 增加list集合為pizza ItemCount取得資料的list的量有多少 利用updataView方法來顯示onBindViewHolder的資料內容

--- app/src/main/java/com/example/eatpizza/MainAdapter.kt
+++ app/src/main/java/com/example/eatpizza/MainAdapter.kt
@@ -3,11 +3,13 @@ package com.example.eatpizza
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
+import com.example.eatpizza.data.Pizza
 
 class MainAdapter(val launchCreatorActivity: (Int?) -> Unit):RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+    val list = mutableListOf<Pizza>()
     inner class ViewHolder (view: View):RecyclerView.ViewHolder(view){
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -15,11 +17,10 @@ class MainAdapter(val launchCreatorActivity: (Int?) -> Unit):RecyclerView.Adapte
         return ViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getItemCount() = list.size
+//    取得list的數量
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.updataView(position)
     }
 }
