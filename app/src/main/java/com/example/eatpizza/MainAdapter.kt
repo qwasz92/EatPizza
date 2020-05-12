27c3ecdee0ecaf1d00c242e59e3a22a2ca9ad00d
Hsuan-Yi,Lin <lovevul35j6@gmail.com>
2020-05-12 16:44:08 +0800

    修正MainAdapter 開啟onCreateViewHolder，回傳ViewHolder 取得getItemCount回傳list資料數量 按下onBindViewHolder，holder使用updateView方法。 建立inner class ViewHolder，裡面有一個updateView方法

--- app/src/main/java/com/example/eatpizza/MainAdapter.kt
+++ app/src/main/java/com/example/eatpizza/MainAdapter.kt
@@ -6,21 +6,43 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
 import com.example.eatpizza.data.Pizza
+import kotlinx.android.synthetic.main.pizza_row.view.*
+import kotlinx.android.synthetic.main.topping_row.view.*
 
-class MainAdapter(val launchCreatorActivity: (Int?) -> Unit):RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+class MainAdapter(val launchCreatorActivity:(Int?) ->Unit):RecyclerView.Adapter<MainAdapter.ViewHolder>() {
     val list = mutableListOf<Pizza>()
-    inner class ViewHolder (view: View):RecyclerView.ViewHolder(view){
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.pizza_row,parent,false)
-        return ViewHolder(view)
+    return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.pizza_row,parent,false))
     }
-
-    override fun getItemCount() = list.size
-//    取得list的數量
-
+    override fun getItemCount(): Int {
+        return list.size
+    }
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.updataView(position)
+        holder.updateView(position)
     }
+
+    inner class ViewHolder(val view: View): RecyclerView.ViewHolder(view){
+        var textView = view.tv_name
+        var button = view.button
+        var divider = view.divider
+        fun updateView(index:Int){
+            if (index != list.size){
+                textView.visibility = View.VISIBLE
+                button.visibility = View.GONE
+                divider.visibility = View.VISIBLE
+
+                textView.text = list[index].name
+                view.setOnClickListener {
+                    launchCreatorActivity(list[index].id)
+                }
+            }else{
+                textView.visibility = View.GONE
+                button.visibility =View.VISIBLE
+                divider.visibility = View.GONE
+
+                button.setOnClickListener { launchCreatorActivity(null) }
+            }
+        }
+}
 }
