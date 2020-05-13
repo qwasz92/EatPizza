6fdd9e0c4df72044283a8701f52b5a3e81fa2519
Hsuan-Yi,Lin <lovevul35j6@gmail.com>
2020-05-13 10:51:14 +0800

    增加Topping內容 建立Topping的id名稱、nam字串、drawableName圖檔字串名稱

--- app/src/main/java/com/example/eatpizza/data/Topping.kt
+++ app/src/main/java/com/example/eatpizza/data/Topping.kt
@@ -1,2 +1,12 @@
 package com.example.eatpizza.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Topping(
+    @PrimaryKey
+    val id:Int,
+    val name:String,
+    val drawableName:String
+)
