d7c97c919e07e1576c43a379cd78af6bba6fff0f
Hsuan-Yi,Lin <lovevul35j6@gmail.com>
2020-05-13 10:55:13 +0800

    建立ToppingDao 建立取得全部資料方法 資料的取得是用id來取得 建立insert方法

--- app/src/main/java/com/example/eatpizza/data/ToppingDao.kt
+++ app/src/main/java/com/example/eatpizza/data/ToppingDao.kt
@@ -1,2 +1,18 @@
 package com.example.eatpizza.data
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface ToppingDao{
+    @Query("select*from topping")
+    fun getAll():List<Topping>
+
+    @Query("select*from topping where id = :id")
+    fun getToppingById(id:Int):Topping
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(topping: Topping)
+}
